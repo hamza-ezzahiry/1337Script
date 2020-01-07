ae5a2bb8057dc7c795f8429360c2b1e9d05dd2b4
Hamza Ezzahiry <hezzahir@e2r9p11.1337.ma>
2020-01-07 11:34:29 +0100

    add script

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-#Author OMB 
+#Author SpiderX
 
 
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = './images/meliodas4K_1.jpg'";
+#
+## Background image
+#
+
+path_image="'$(pwd)/images/meliodas4K_1.jpg'"
+
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = ${path_image}";
 killall Dock;
+
+defaults write com.apple.screensaver askForPassword -int 0
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write -g com.apple.mouse.scaling 3
